f972dedadaaca02a209a6174265fa4156bef924e
ST LEON <leonst998@gmail.com>
2014-04-14 19:13:52 +0400

    v0.0.1
    happy birthday!

---
+++ 2d3.py
@@ -0,0 +1,56 @@
+import json
+from main import VkFriends
+from settings import token, my_id, api_v
+
+
+class D3(VkFriends):
+	"""
+	Генерит json, дабы можно было заюзать http://bl.ocks.org/mbostock/4062045
+	{
+		"nodes":[
+			{"name":"Myriel","group":1},
+			{"name":"Napoleon","group":1},
+			{"name":"Mlle.Baptistine","group":1}
+		],
+		"links":[
+			{"source":1,"target":0,"value":1},
+			{"source":2,"target":0,"value":8}
+		]
+	}
+	target - Myriel, source 1 - Napoleon
+	"""
+
+	def __init__(self, token, my_id, api_v):
+		VkFriends.__init__(self, token, my_id, api_v)
+		self.js = {"nodes": [], "links": []}
+		self.write_json(self.to_json())
+
+	def to_json(self):
+		"""
+		Из self.friendships сначала составляем узлы, затем ребра
+		Если одновременно, то ребра вначале могут ссылаться на несуществующие узлы
+		Group везде одинаковый, но добавлено photo
+		"""
+		for i in self.friendships:
+			self.js['nodes'].append({"name": "%s %s" % (i[0]['first_name'], i[0]['last_name']),
+									 												"group": 1, "photo": i[0]['photo']})
+		for i in self.friendships:
+			if i[1]:
+				find_world = '%s %s' % (i[0]['first_name'], i[0]['last_name'])
+				for d in self.js["nodes"]:
+					if find_world in d.values():
+						for c in i[1]:
+							find_friend = '%s %s' % (c['first_name'], c['last_name'])
+							for e in self.js["nodes"]:
+								if find_friend in e.values():
+									self.js['links'].append({"source": self.js["nodes"].index(e),
+															 		"target": self.js["nodes"].index(d), "value": 1})
+
+		return json.JSONEncoder().encode(self.js)
+
+	def write_json(self, json):
+		with open("web/miserables.json","w") as f:
+			f.write(json)
+
+if __name__ == '__main__':
+	a = D3(token, my_id, api_v)
---
+++ main.py
@@ -0,0 +1,60 @@
+import sys
+import requests
+from settings import token, my_id, api_v
+
+
+class VkFriends():
+	"""
+	Находит друзей, находит общих друзей
+	"""
+
+	def __init__(self, token, my_id, api_v):
+		self.token, self.my_id, self.api_v = token, my_id, api_v
+		my_inf = self.base_info([self.my_id,])[0]
+		self.my_name, self.my_last_name, self.photo = my_inf['first_name'], my_inf['last_name'], my_inf['photo']
+		self.all_friends = self.friends(self.my_id)
+		self.friendships = list(map(lambda x:self.common_friends(self.my_id, x), self.all_friends))
+
+	def request_url(self, method_name, parameters):
+		"""read https://vk.com/dev/api_requests"""
+		return 'https://api.vk.com/method/{method_name}?{parameters}&v={api_v}&access_token={token}'.format(
+			method_name=method_name, api_v=self.api_v,parameters=parameters, token=self.token)
+
+	def base_info(self, ids):
+		"""read https://vk.com/dev/users.get"""
+		r = requests.get(self.request_url('users.get', 'user_ids=%s&fields=photo' %
+														  (','.join(map(str, ids))))).json()['response']
+		# Проверяем, если id из settings.py не деактивирован
+		if 'deactivated' in r[0].keys():
+			sys.exit("User deactivated")
+		return r
+
+	def friends(self, id):
+		"""
+		read https://vk.com/dev/users.get
+		Принимает идентификатор пользователя
+		Возвращает список активных анкет с инфой
+		"""
+		r = requests.get(self.request_url('friends.get',
+										  'user_id=%s&fields=uid,first_name,last_name,photo' % id)).json()['response']
+		#self.count_friends = r['count']
+		# удаляем деактивированные аккаунты
+		return list(filter((lambda x: 'deactivated' not in x.keys()), r['items']))
+
+	def common_friends(self, source, target):
+		"""
+		read https://vk.com/dev/friends.getMutual
+		Принимает идентификатор источника и ифну о цели
+		Возвращает в кортеже инфу о цели и список общих друзей с инфой
+		"""
+		r = requests.get(self.request_url('friends.getMutual',
+										  'source_uid=%s&target_uid=%s' % (source, target['id']) )).json()
+		print(r)
+		# return target, self.base_info(r['response']) if r['response'] else None # почувствуй разницу
+		# берем инфу из уже полного списка
+		return target, [id for i in r['response'] for id in self.all_friends if i == id['id']] if r['response'] else None
+
+if __name__ == '__main__':
+	a = VkFriends(token, my_id, api_v)
+	print(a.my_name, a.my_last_name, a.my_id, a.photo)
+	print(a.friendships)
